7102138e3ed68e3d8d15d5cab7f840410a05d5cb
russmack <russ_mack@hotmail.com>
2015-04-21 16:06:47 +0100

    Comments.

--- examples/example.go
+++ examples/example.go
@@ -1,3 +1,4 @@
+// This is an example usage of the HackerNews api cliient library.
 package main
 
 import (
@@ -6,22 +7,24 @@ import (
 )
 
 func main() {
+	// Create a client.
 	client := hackernews.NewClient()
+	// Get ids of top stories.
 	stories, err := client.GetTopStories()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("Got stories: %v\n", stories)
-
+	// Get the top story.
 	topStoryId := stories[0]
 	fmt.Println("Top story id:", topStoryId)
-
 	story, err := client.GetItem(topStoryId)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// Display the top story.
 	fmt.Printf("Top story: %v\n", story)
 	fmt.Println("Title:", story.Title)
 	fmt.Println("Url:", story.Url)
